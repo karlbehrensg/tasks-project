a546d3055d2616f115b4b147cff416665cf8c58a
Karl Behrens <karlbehrensg@gmail.com>
2020-09-26 22:04:03 -0300

    Elementos de navbar

---
+++ front/home_office/components/Navbar/index.js
@@ -0,0 +1,54 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <h1>Home Office</h1>
+        <div className="menu">
+          <div className="menu_tareas">
+            <h5>Buscar tareas</h5>
+            <h5>Publicar</h5>
+          </div>
+          <div className="menu_perfil">
+            <h5>Iniciar sesion</h5>
+            <h5>Registrate</h5>
+          </div>
+        </div>
+      </nav>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          align-items: center;
+          border: 1px red solid;
+          height: 64px;
+        }
+
+        h1 {
+          margin-left: 20px;
+          min-width: 150px;
+          width: 10%;
+        }
+
+        .menu {
+          display: flex;
+          justify-content: space-between;
+          width: 100%;
+        }
+
+        .menu_tareas {
+          display: flex;
+          margin: 10px;
+          justify-content: space-between;
+          width: 180px;
+        }
+
+        .menu_perfil {
+          display: flex;
+          margin: 10px;
+          justify-content: space-between;
+          width: 180px;
+        }
+      `}</style>
+    </>
+  )
+}
