6bf85d5780b8951774be2d65bdd525174d324908
Karl Behrens <karlbehrensg@gmail.com>
2020-09-27 00:42:44 -0300

    Formulario de registro

--- front/home_office/pages/register/index.js
+++ front/home_office/pages/register/index.js
@@ -1,7 +1,34 @@
 export default function Register() {
   return (
     <>
-      <h1>Register</h1>
+      <div className="main">
+        <div className="form_container">
+          <form>
+            <div>
+              <input placeholder="Nombre..." />
+              <input placeholder="Apellido paterno..." />
+              <input placeholder="Apellido materno..." />
+            </div>
+            <div>
+              <input placeholder="Correo eletronico..." />
+              <input placeholder="Confirmar correo..." />
+            </div>
+            <div>
+              <input placeholder="Contraseña..." />
+              <input placeholder="Confirmar contraseña..." />
+            </div>
+            <button>Registrarte</button>
+          </form>
+        </div>
+
+        <style jsx>{`
+          .form_container {
+            display: flex;
+            width: 500px;
+            border: 2px red solid;
+          }
+        `}</style>
+      </div>
     </>
   )
 }
